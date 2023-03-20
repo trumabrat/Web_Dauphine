dfd239d70163a7a815e314fe1171e2456ea4c412
Mihajlo Trumic <trumic.eleve@gmail.com>
2023-03-20 01:44:38 +0100

    final modification

--- ProblemSet4/forum/posts/views.py
+++ ProblemSet4/forum/posts/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Post
+
 
 def index(request):
-    return render(request, 'home.html')
+    posts = Post.objects.all()
+    posts = posts.order_by('-created_at')
+    return render(request, 'home.html', {'posts': posts})
+
 
 def add(request):
     if request.method == 'POST':
         author = request.POST['author']
-        body = request.POST['body']
+        author = author[0].upper() + author[1:]
+
+        body = request.POST['content']
+
+        if (len(author) > 20 or len(body) < 10 or len(body) > 120):
+            return HttpResponse("Invalid entry. Author's name can't be more than 20 characters. The post must contain between 10 and 120 characters")
         post = Post(author=author, body=body)
         post.save()
         return redirect("/")
-    
+
     return render(request, 'add.html')
 
-def get_entry(request,post_id):
-    return HttpResponse(f"Hello, world. You're at the posts get_entry. Post id is {post_id}.")
+
+def get_entry(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return HttpResponse(post)
